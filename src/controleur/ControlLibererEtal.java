ef9ab2923ea226df6f90fe03b80d43515cf8b813
D33333 <dc3comics@gmail.com>
2023-10-25 10:57:28 +0200

    TP3_cas7 terminé

--- src/controleur/ControlLibererEtal.java
+++ src/controleur/ControlLibererEtal.java
@@ -1,5 +1,7 @@
 package controleur;
 
+import villagegaulois.Etal;
+
 public class ControlLibererEtal {
 	private ControlTrouverEtalVendeur controlTrouverEtalVendeur;
 
@@ -8,7 +10,9 @@ public class ControlLibererEtal {
 		this.controlTrouverEtalVendeur = controlTrouverEtalVendeur;
 	}
 
-	//TODO a completer
+	public boolean isVendeur(String nomVendeur) {
+		return (controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur) != null);
+	}
 
 	/**
 	 * 
@@ -19,9 +23,14 @@ public class ControlLibererEtal {
 	 *         quantité de produit vendu
 	 */
 	public String[] libererEtal(String nomVendeur) {
-		//TODO a completer
-		String[] donneesEtal = null;
+		Etal etal = controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur);
+		String[] donneesEtal;
+		if (etal == null) {
+			donneesEtal = new String[]{"false","","",""};
+		} else {
+			donneesEtal = etal.etatEtal();
+		}
 		return donneesEtal;
 	}
-
+	
 }
